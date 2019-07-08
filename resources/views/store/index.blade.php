96a1564fd69504e6e042694b9c192f20c093195f
Natdanai Jampasri <ohking74@gmail.com>
2019-07-08 15:07:58 +0700

    icon change

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -1,6 +1,14 @@
 @extends('Home.master')
 @section('title','welcome Homepage')
 @section('content')
+<script>
+  $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $('.test').click(function(){
+      $(this).next('form').submit();
+    });
+  });
+</script>
 <div class="container">
   <div class="row">
     <div class="col-md-12"> <br />
@@ -12,15 +20,9 @@
       <div class="card">
         <div class="card-header" style="background-color:#435d7d;">
           <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-10">
               <h3 class="text-white">STORES</h3>
             </div>
-            <div class="col-md-2 text-right">
-              <a class="btn btn-danger text-white text-right" href="{{route('store.create')}}">
-              <i class="fas fa-trash"></i>
-              ลบร้านค้า
-              </a>
-            </div> 
             <div class="col-md-2 text-right">
               <a class="btn btn-success text-white text-right" href="{{route('store.create')}}">
               <i class="fas fa-plus"></i>
@@ -28,38 +30,45 @@
               </a>
             </div>  
           </div>
-            
         </div>
         <div class="card-body">
-          <table class="table table-hover text-center">
-            <tr>
-              <th>ลำดับ</th>
-              <th>รหัสร้านค้า</th>
-              <th>ร้านค้า</th>
-              <th>แก้ไข</th>
-              <th>ลบร้านค้า</th>
-              <th>ประวัติร้านค้า</th>
-            </tr>
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th style="width:10%;">#</th>
+                <th style="width:20%;">รหัสร้านค้า</th>
+                <th style="width:40%;">ร้านค้า</th>
+                <th colspan="3">Manage</th>
+              </tr>
+            </thead>
+            <tbody>
             @foreach($store as $row)
             <tr>
               <td>{{$row['id']}}</td>
               <td>{{$row['keystore']}}</td>
               <td>{{$row['name']}}</td>
-              <td><a href="{{action('StoreController@edit',$row['id'])}}" class="btn btn-sm btn-primary">Edit</a></td>
-              <td>
-                <form method="post" class="delete_form" action="{{action('StoreController@destroy',$row['id'])}}">
-                  {{csrf_field()}}
-                <input type="hidden" name="_method" value="DELETE" />
-                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-              </form>
-              </td>
-              <td><a href="{{action('StoreController@show',$row['id'])}}" class="btn btn-primary btn-sm">Show</a></td>
-            </tr>
+              <td colspan="3">
+                    <a href="{{action('StoreController@show',$row['id'])}}" data-toggle="tooltip" data-placement="top" title="View"><i style="font-size:20px;;" class="fas fa-eye text-primary"></i></a>
+                    &nbsp;&nbsp;
+                    <a href="{{action('StoreController@edit',$row['id'])}}" data-toggle="tooltip" data-placement="top" title="Edit"><i style="font-size:20px;" class="fas fa-edit text-warning"></i></a>
+                    &nbsp;&nbsp;
+                    <a class="test" href="#" data-toggle="tooltip" data-placement="top" title="Remove"><i style="font-size:20px;" class="fas fa-trash-alt text-danger"></i></a>
+                    <form method="post" class="delete_form" action="{{action('StoreController@destroy',$row['id'])}}">
+                      {{csrf_field()}}
+                      <input type="hidden" name="_method" value="DELETE" />
+                    </form>
+                  </tr>
+                </div>
             @endforeach
+            </tbody>
           </table>
         </div>
       </div>
     </div>
   </div>
 </div>
+
+  
 @stop
+
+
