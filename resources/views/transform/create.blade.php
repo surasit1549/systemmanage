2b179aec2dc694561dc2c3c85bfd145db806ba1c
Natdanai Jampasri <ohking74@gmail.com>
2019-07-18 20:04:35 +0700

    nothing

--- resources/views/transform/create.blade.php
+++ resources/views/transform/create.blade.php
@@ -10,29 +10,32 @@
 @stop
 @section('content')
 <div class="container">
-  <div class="row">
-    <div class="col-md-12"> <br />
-      <h3 align="center">เพิ่มข้อมูลแปลง</h3> <br />
-      <form method="post" action="{{url('transform')}}">
-        {{csrf_field()}}
-        <div class="form-group">
-          <table style="width:100%">
-            <tr>
-                <td>ชื่อแปลง :</td>
-                <td><input type="text" name="convertname" class="form-control"/></td>
-            </tr>
-            <tr>
-              <div class="form-group">
-                <td>ขนาด :</td>
-                <td><input type="text" name="size" class="form-control"  /></td>
-              </div>
-            </tr>
-          </table>
-        </div>
-        <div class="form-group">
-          <input type="submit" class="btn btn-primary" value="save" />
-        </div>
-      </form>
+  <div class="card">
+    <div style="background-color:#435d7d" class="card-header text-white">
+      <h3>เพิ่มข้อมูลแปลง</h3>
+    </div>
+    <div class="card-body">
+        <form method="post" action="{{url('transform')}}">
+          {{csrf_field()}}
+          <div class="form-group">
+            <table style="width:100%">
+              <tr>
+                  <td>ชื่อแปลง</td>
+                  <td><input type="text" name="convertname" class="form-control"/></td>
+              </tr>
+              <tr>
+                <div class="form-group">
+                  <td>ขนาด</td>
+                  <td><input type="text" name="size" class="form-control"  /></td>
+                </div>
+              </tr>
+            </table>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-danger"><i class="fas fa-undo"></i>&nbsp;&nbsp;ย้อนกลับ</button>
+            <button type="submit" class="btn btn-success"><i class="far fa-save"></i>&nbsp;&nbsp;บันทึก</button>
+          </div>
+        </form>
     </div>
   </div>
 </div>
