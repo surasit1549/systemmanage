4aad7b28544d8428439b9293ab123c88ec58f75d
surasit <surasit1549@gmail.com>
2019-09-05 14:26:21 +0700

    pdf

--- app/Http/Controllers/PurchaseorderController.php
+++ app/Http/Controllers/PurchaseorderController.php
@@ -12,6 +12,7 @@ use App\checkkeystore;
 use App\Authorized_person2;
 use App\pr_store;
 use vendor\autoload;
+use App\pr_create;
 
 class PurchaseorderController extends Controller
 {
@@ -51,7 +52,7 @@ class PurchaseorderController extends Controller
      */
     public function create()
     {
-        return view('porder.create');
+        //
     }
 
     /**
@@ -71,21 +72,71 @@ class PurchaseorderController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    function sum_price($sumofprice)
+    {
+        $sum = number_format(($sumofprice * (100 / 107)), 2, '.', '');
+        return $sum;
+    }
+
+    function tax($sum_price, $sumofprice)
+    {
+        $tax = floatval($sumofprice) - $sum_price;
+        $str_tax = number_format($tax, 2, '.', '');
+        return $tax;
+    }
+
+    function bathformat($number)
+    {
+        $numberstr = array('ศูนย์', 'หนึ่ง', 'สอง', 'สาม', 'สี่', 'ห้า', 'หก', 'เจ็ด', 'แปด', 'เก้า', 'สิบ');
+        $digitstr = array('', 'สิบ', 'ร้อย', 'พัน', 'หมื่น', 'แสน', 'ล้าน');
+
+        $number = str_replace(",", "", $number); // ลบ comma
+        $number = explode(".", $number); // แยกจุดทศนิยมออก
+
+        // เลขจำนวนเต็ม
+        $strlen = strlen($number[0]);
+        $result = '';
+        for ($i = 0; $i < $strlen; $i++) {
+            $n = substr($number[0], $i, 1);
+            if ($n != 0) {
+                if ($i == ($strlen - 1) and $n == 1) {
+                    $result .= 'เอ็ด';
+                } elseif ($i == ($strlen - 2) and $n == 2) {
+                    $result .= 'ยี่';
+                } elseif ($i == ($strlen - 2) and $n == 1) {
+                    $result .= '';
+                } else {
+                    $result .= $numberstr[$n];
+                }
+                $result .= $digitstr[$strlen - $i - 1];
+            }
+        }
+        $result .= 'บาทถ้วน';
+        return $result;
+    }
+
     public function show($id)
     {
         $number = 1;
         $po_id = porder::find($id);
-
-        $data = pr_store::where('PO_ID',$po_id['PO_ID'])->get()->toArray();
-        $store = Store::where('keystore',$po_id['store_ID'])->get()->toArray();
+        $convert = pr_create::where('key', $po_id['keyPR'])->get();
+        $data = pr_store::where('PO_ID', $po_id['PO_ID'])->get()->toArray();
         //dd($data[0]['sumofprice']);
-        $store_mine = Store::where('keystore','master')->get();
-
+        $sum_price = $this->sum_price($data[0]['sumofprice']);
+        $tax = $this->tax($sum_price, $data[0]['sumofprice']);
+        $letter_sumofprice = $this->bathformat($data[0]['sumofprice']);
+        $store = Store::where('keystore', $po_id['store_ID'])->get()->toArray();
+        $store_mine = Store::where('keystore', 'master')->get();
         return view('porder.show', compact(
             'po_id',
             'data',
             'store',
             'store_mine',
+            'convert',
+            'sum_price',
+            'tax',
+            'letter_sumofprice',
             'number',
             'id'
         ));
--- resources/views/porder/show.blade.php
+++ resources/views/porder/show.blade.php
@@ -133,8 +133,142 @@
   </div>
 </div>
 
-<div id="exportpdf">
-  
+<div id="exportpdf" class="d-none">
+  <div id="cover_detail_table">
+    {{$store_mine[0]['name']}} <br>
+    {{$store_mine[0]['address']}} <br>
+    เบอร์โทรศัทพ์ {{$store_mine[0]['phone']}}
+  </div>
+  <div id="title_table">
+    <h2>ใบสั่งซื้อ</h2>
+  </div>
+  <div id="store_ID_table">
+    <table>
+      <th>เลขที่เอกสาร PO</th>
+      <th>{{$po_id['PO_ID']}}</th>
+    </table>
+  </div>
+  <div id="store_table">
+    <table id="table_store">
+      <tr>
+        <th> ผู้ขาย :</th>
+        <td>
+          {{$store[0]['name']}}&nbsp;&nbsp; [ {{$store[0]['keystore']}} ]
+        </td>
+        <th> วันที่เอกสาร :</th>
+        <td>
+          {{substr($data[0]['created_at'],0,-9)}}
+        </td>
+      </tr>
+      <tr>
+        <th> ที่อยู่ :</td>
+        <td>
+          {{$store[0]['address']}}
+        </td>
+        <th> ผู้ติดต่อ : </th>
+        <td>
+          {{$store[0]['contect']}} &nbsp;&nbsp; {{$store[0]['cellphone']}}
+        </td>
+      </tr>
+      <tr>
+        <th> โทรศัพท์ :</th>
+        <td>
+          {{$store[0]['phone']}}
+        </td>
+        <th> วันที่กำหนดส่ง : </th>
+        <td>
+
+        </td>
+      </tr>
+      <tr>
+        <th> โทรสาร : </th>
+        <td>
+          {{$store[0]['fax']}}
+        </td>
+        <th> จำนวนเครดิต : </th>
+        <td>
+
+        </td>
+      </tr>
+      <tr>
+        <th> </th>
+        <td> </td>
+        <th> เงื่อนไขการชำระ : </th>
+        <td>
+
+        </td>
+      </tr>
+    </table>
+  </div>
+  <div id="product_table">
+    <table id="table_product">
+      <thead>
+        <tr>
+          <th>ลำดับที่</th>
+          <th>ชื่อสินค้า</th>
+          <th>จำนวนสินค้า</th>
+          <th>หน่วย</th>
+          <th>ราคา (บาท)</th>
+          <th>จำนวนเงิน</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($data as $num=>$row)
+        <tr>
+          <td style="width: 5%"><label>{{$num+1}}</label></td>
+          <td style="width: 25%"><label value="{{$row['Product_name']}}" name="" required>{{$row['Product_name']}}</label>
+          <td style="width: 10%"><label value="{{$row['Product_number']}}" name="" required>{{$row['Product_number']}}</label></td>
+          <td style="width: 10%"><label value="{{$row['unit']}}" name="" required>{{$row['unit']}}</label>
+          <td style="width: 10%"><label value="{{$row['price']}}" required>{{$row['price']}}</label></td>
+          <td style="width: 15%"><label value="{{$row['product_sum']}}" required>{{$row['product_sum']}}</label></td>
+        </tr>
+        @endforeach
+        @for($i = $number ; $i <= 10 ; $i++) <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          </tr>
+          @endfor
+      </tbody>
+      <tfoot>
+        <tr>
+          <th style="vertical-align:top" rowspan="3">หมายเหตุ</th>
+          <td style="vertical-align:top" rowspan="3" colspan="3">สำหรับ &nbsp; {{$convert[0]['prequestconvert']}}&nbsp;PR &nbsp;{{$po_id['keyPR']}} <br> ส่งสินค้า ถนนวงแหวนรอบ3 หลังอรสิริน บิสสิเนส 3 (แยกท่ารั้ว)<br>ติดต่อสั่งชื้อ {{$store_mine[0]['contect']}}&nbsp; {{$store_mine[0]['cellphone']}} </td>
+          <th>ราคายังไม่รวมภาษี</th>
+          <td style="text-align:center">{{$sum_price}}</td>
+        </tr>
+        <tr>
+          <th>ส่วนลด</th>
+          <td></td>
+        </tr>
+        <tr>
+          <th>ภาษีมูลค่าเพิ่ม 7%</th>
+          <td style="text-align:center">{{$tax}}</td>
+        </tr>
+        <tr>
+          <th>ตัวอักษร</th>
+          <td colspan="3">&nbsp;&nbsp;{{$letter_sumofprice}}</td>
+          <th>รวมทั้งสิ้น</th>
+          <td style="text-align:center">{{$data[0]['sumofprice']}}</td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
+  <div id="signature_1">
+    <img id="signatureimg" src="{{Auth::user()->signature}}" alt="">
+    <h4>{{ Auth::user()->firstname }}&nbsp;&nbsp;{{ Auth::user()->lastname }}<br>{{ Auth::user()->role }}<br>วันที่ {{ date('d-m-Y') }}</h4>
+  </div>
+  <div id="signature_2">
+    <img id="signatureimg" src="{{Auth::user()->signature}}" alt="">
+    <h4>{{ Auth::user()->firstname }}&nbsp;&nbsp;{{ Auth::user()->lastname }}<br>{{ Auth::user()->role }}<br>วันที่ {{ date('d-m-Y') }}</h4>
+  </div>
+  <div id="contrator">
+    <img id="signatureimg" src="{{Auth::user()->signature}}" alt="">
+    <h4>{{ Auth::user()->firstname }}&nbsp;&nbsp;{{ Auth::user()->lastname }}<br>{{ Auth::user()->role }}<br>วันที่ {{ date('d-m-Y') }}</h4>
+  </div>
 </div>
 
-  @endsection
+@endsection
